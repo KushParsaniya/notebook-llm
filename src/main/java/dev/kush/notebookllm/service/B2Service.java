695355714fac7418acec5bdb320a7c82a5070282
kush parsaniya <kushparsaniya94@gmail.com>
2024-12-20 19:55:56 +0530

    Packaging change to war.

--- src/main/java/dev/kush/notebookllm/service/B2Service.java
+++ src/main/java/dev/kush/notebookllm/service/B2Service.java
@@ -1,11 +1,12 @@
 package dev.kush.notebookllm.service;
 
+import dev.kush.notebookllm.controller.UserController;
 import dev.kush.notebookllm.dto.CustomB2ContentSource;
 
 public interface B2Service {
-    boolean uploadSmallFileToBucket(CustomB2ContentSource customB2ContentSource);
+    UserController.UserUploadedFileDto uploadSmallFileToBucket(CustomB2ContentSource customB2ContentSource);
 
-    boolean uploadLargeFileToBucket(CustomB2ContentSource customB2ContentSource);
+    UserController.UserUploadedFileDto uploadLargeFileToBucket(CustomB2ContentSource customB2ContentSource);
 
     byte[] downloadFile(String fileId);
 
--- src/main/java/dev/kush/notebookllm/service/impl/B2ServiceImpl.java
+++ src/main/java/dev/kush/notebookllm/service/impl/B2ServiceImpl.java
@@ -6,11 +6,15 @@ import com.backblaze.b2.client.contentSources.B2ContentTypes;
 import com.backblaze.b2.client.exceptions.B2Exception;
 import com.backblaze.b2.client.structures.B2FileVersion;
 import com.backblaze.b2.client.structures.B2UploadFileRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.kush.notebookllm.controller.UserController;
 import dev.kush.notebookllm.dto.CustomB2ContentSource;
 import dev.kush.notebookllm.entity.UploadedFile;
 import dev.kush.notebookllm.enums.UploadedFileStatus;
 import dev.kush.notebookllm.service.B2Service;
+import dev.kush.notebookllm.service.UploadedFileService;
 import dev.kush.notebookllm.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,6 +26,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Service
+@RequiredArgsConstructor
 public class B2ServiceImpl implements B2Service {
 
     private static final Logger log = LoggerFactory.getLogger(B2ServiceImpl.class);
@@ -29,19 +34,14 @@ public class B2ServiceImpl implements B2Service {
     private final ExecutorService executorService;
     private final ApplicationEventPublisher applicationEventPublisher;
     private final UserService userService;
+    private final UploadedFileService uploadedFileService;
+    private final ObjectMapper objectMapper;
 
     @Value("${b2.bucket.id}")
     private String bucketId;
 
-    public B2ServiceImpl(B2StorageClient b2StorageClient, ApplicationEventPublisher applicationEventPublisher, UserService userService) {
-        this.b2StorageClient = b2StorageClient;
-        this.executorService = Executors.newVirtualThreadPerTaskExecutor();
-        this.applicationEventPublisher = applicationEventPublisher;
-        this.userService = userService;
-    }
-
     @Override
-    public boolean uploadSmallFileToBucket(CustomB2ContentSource customB2ContentSource) {
+    public UserController.UserUploadedFileDto uploadSmallFileToBucket(CustomB2ContentSource customB2ContentSource) {
         var b2UploadFileRequest = B2UploadFileRequest
                 .builder(bucketId,
                         customB2ContentSource.getFile().getName().replace(".pdf", "") + "-" + UUID.randomUUID() + ".pdf",
@@ -49,13 +49,15 @@ public class B2ServiceImpl implements B2Service {
                         customB2ContentSource)
                 .build();
         try {
-            var uploadedFile = b2StorageClient.uploadSmallFile(b2UploadFileRequest);
-            applicationEventPublisher.publishEvent(getUploadedFile(customB2ContentSource, uploadedFile));
+            var b2FileVersion = b2StorageClient.uploadSmallFile(b2UploadFileRequest);
+            var uploadedFile = getUploadedFile(customB2ContentSource, b2FileVersion);
+            uploadedFile = uploadedFileService.save(uploadedFile);
+            applicationEventPublisher.publishEvent(uploadedFile);
             log.info("File uploaded successfully");
-            return true;
+            return getUserUploadedFileDto(uploadedFile);
         } catch (B2Exception e) {
             e.printStackTrace();
-            return false;
+            return null;
         }
     }
 
@@ -73,7 +75,7 @@ public class B2ServiceImpl implements B2Service {
     }
 
     @Override
-    public boolean uploadLargeFileToBucket(CustomB2ContentSource customB2ContentSource) {
+    public UserController.UserUploadedFileDto uploadLargeFileToBucket(CustomB2ContentSource customB2ContentSource) {
         var b2UploadFileRequest = B2UploadFileRequest
                 .builder(bucketId,
                         customB2ContentSource.getFile().getName().replace(".pdf", "") + "-" + UUID.randomUUID() + ".pdf",
@@ -82,14 +84,20 @@ public class B2ServiceImpl implements B2Service {
                 .build();
 
         try {
-            var uploadedFile = b2StorageClient.uploadLargeFile(b2UploadFileRequest, executorService);
-            applicationEventPublisher.publishEvent(getUploadedFile(customB2ContentSource, uploadedFile));
+            var b2FileVersion = b2StorageClient.uploadLargeFile(b2UploadFileRequest, executorService);
+            var uploadedFile = getUploadedFile(customB2ContentSource, b2FileVersion);
+            uploadedFile = uploadedFileService.save(uploadedFile);
+            applicationEventPublisher.publishEvent(uploadedFile);
             log.info("File uploaded successfully");
-            return true;
+            return getUserUploadedFileDto(uploadedFile);
         } catch (B2Exception e) {
             e.printStackTrace();
-            return false;
+            return null;
         }
+    }
+
+    private UserController.UserUploadedFileDto getUserUploadedFileDto(UploadedFile uploadedFile) {
+        return objectMapper.convertValue(uploadedFile, UserController.UserUploadedFileDto.class);
     }
 
     @Override
